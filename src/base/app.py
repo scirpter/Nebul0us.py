9ddd26ba8ac930e09368ed151889fb768b6682b0
Sascha <nyaanity@gmail.com>
2023-12-09 19:20:40 +0100

    removed event handler (todo: iterate through plugins and its functions

--- src/base/app.py
+++ src/base/app.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 from types import ModuleType
 from typing import Any, Callable
-from base.event_handler import EventHandler, ScriptEvent
 from base.gowire import Gowire
-from helpers.plugins import PluginRegistry
+from helpers.plugins import PluginRegistry, ScriptEvent
 from game.models.client import Client
 from importlib import import_module
 from importlib.util import find_spec
@@ -14,18 +13,10 @@ class App:
     def __init__(self) -> None:
         self.wire: Gowire = Gowire()
         self.plugin_registry: PluginRegistry = PluginRegistry()
-        self.event_handler: EventHandler = EventHandler()
         self.clients: list[Client] = []
 
     def dispatch(self, t: ScriptEvent, *args: ..., **kwargs: ...) -> None:  # type: ignore
-        if t not in self.event_handler.event_handlers:
-            return
-        for callback in self.event_handler.event_handlers[t]:
-            callback(*args, **kwargs)
-
-    def on(self, t: ScriptEvent, fun: Callable[..., Any]) -> App:
-        self.event_handler.register(t, fun)
-        return self
+        ...
 
     def register_plugins(self) -> App:
         for path in Path("src/plugins").iterdir():
